6c41d3c8badc59a9c55c19fd60611e4b7343c135
TalhaBodla509 <zaidbodla786@gmail.com>
2024-12-11 14:42:42 +0500

    Add files via upload

---
+++ WpfApp4/WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Combo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (comboBox.SelectedItem is ComboBoxItem selectedItem)
+            {
+                textBox.Text = selectedItem.Content.ToString();
+            }
+            else
+            {
+                textBox.Text = string.Empty;
+            }
+        }
+
+        private void Combo1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (comboBox1.SelectedItem is ComboBoxItem selectedItem)
+            {
+                textBox1.Text = selectedItem.Content.ToString();
+            }
+            else
+            {
+                textBox1.Text = string.Empty;
+            }
+        }
+    } 
+}
